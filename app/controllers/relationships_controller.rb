839ddad4db796919a2f465053de9e2ef092f9513
EC2 Default User <ec2-user@ip-172-31-46-143.ec2.internal>
2023-05-04 13:32:07 +0000

    evo

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,27 @@
+class RelationshipsController < ApplicationController
+  def create
+   current_user.follow(params[:user_id])
+	 redirect_to request.referer
+  end
+
+  def destroy
+	    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+
+def follow
+    user = User.find(params[:user_id])
+    @users = user.followings
+end
+
+def follower
+    user = User.find(params[:user_id])
+    @users = user.followers
+end
+private
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
+
