c943c1cfbfd42c0e9ad010b158d47396705800d2
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-08 12:57:31 +0000

    fin

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favorite.save
-
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
+    render  'create'
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
 
   end
 end
